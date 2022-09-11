4d012a44a525eec1786e1220abcc20e803fd2c12
Mason Mei <masonmei@pursuit.org>
2022-09-11 17:18:07 -0500

    data

---
+++ back-end/db/prod_seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO profiles (address, name, twitter, instagram, linkedin, picture, about) VALUES
+('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', 'masmei', 'https://twitter.com/Dapp_Boy', 'https://www.instagram.com/masmei/', 'https://www.linkedin.com/in/masonmei/', 'https://picsum.photos/id/1080/300/300', 'I love NFTs!')
+;
