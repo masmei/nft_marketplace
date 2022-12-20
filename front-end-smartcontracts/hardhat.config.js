82629024c65f30b3dc085896e06fd85658390ffe
Mason Mei <masonmei@pursuit.org>
2022-12-19 22:59:15 -0500

    make it better

--- front-end-smartcontracts/hardhat.config.js
+++ front-end-smartcontracts/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
 
 module.exports = {
   solidity: "0.8.4",
@@ -14,5 +16,10 @@ module.exports = {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
   }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "ABZMQ4A2A9HRVKB86K7QDZBWWZ3PBIF7RC"
   }
 };
--- front-end-smartcontracts/src/frontend/components/NewProfileForm.js
+++ front-end-smartcontracts/src/frontend/components/NewProfileForm.js
@@ -40,106 +40,107 @@ function NewProfileForm({ account, loading }) {
     addProfile(profile);
   };
 
-  if (loading) return (
-    <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      minHeight: "80vh",
-    }}
-  >
-    <Spinner animation="border" style={{ display: "flex" }} />
-    <p className="mx-3 my-0">Awaiting Metamask Connection...</p>
-  </div>
-  ) 
+  if (loading)
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "80vh",
+        }}
+      >
+        <Spinner animation="border" style={{ display: "flex" }} />
+        <p className="mx-3 my-0">Awaiting Metamask Connection...</p>
+      </div>
+    );
 
   return (
     <div className="container-fluid mt-5">
-    <h2>Create Profile</h2>
-  <Form onSubmit={handleSubmit}>
-    <div className="row">
-      <main
-        role="main"
-        className="col-lg-12 mx-auto"
-        style={{ maxWidth: "600px" }}
-      >
-        <div className="content mx-auto">
-          <Row className="g-6">
-          <Form.Label>Name</Form.Label>
-            <Form.Control
-            className="mb-2"
-              size="md"
-              type="text"
-              required
-              id="name"
-              value={profile.name}
-              onChange={handleTextChange}
-            />
-            <Form.Label>Twitter</Form.Label>
-            <Form.Control
-            className="mb-2"
-              size="md"
-              type="text"
-              id="twitter"
-              value={profile.twitter}
-              onChange={handleTextChange}
-            />
-            <Form.Label>Instagram</Form.Label>
-            <Form.Control
-            className="mb-2"
-              size="md"
-              type="text"
-              id="instagram"
-              value={profile.instagram}
-              onChange={handleTextChange}
-            />
-            <Form.Label>Linkedin</Form.Label>
-            <Form.Control
-            className="mb-2"
-              size="md"
-              type="text"
-              id="linkedin"
-              value={profile.linkedin}
-              onChange={handleTextChange}
-            />
-            <Form.Label>Profile Picture</Form.Label>
-            <Form.Control
-            className="mb-2"
-              size="md"
-              type="text"
-              id="picture"
-              value={profile.picture}
-              onChange={handleTextChange}
-            />
-            <Form.Label>About Me</Form.Label>
-            <Form.Control
-            className="mb-4"
-              size="md"
-              required
-              as="textarea"
-              id="about"
-              value={profile.about}
-              placeholder="Tell us about yourself!"
-              onChange={handleTextChange}
-            />
-            <div className="d-grid px-0">
-              <Button
-                type="submit"
-                style={{ maxWidth: "200px" }}
-                variant="warning"
-                size="md"
-              >
-                Submit
-              </Button>
+      <h2>Create Profile</h2>
+      <Form onSubmit={handleSubmit}>
+        <div className="row">
+          <main
+            role="main"
+            className="col-lg-12 mx-auto"
+            style={{ maxWidth: "600px" }}
+          >
+            <div className="content mx-auto">
+              <Row className="g-6">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  className="mb-2"
+                  size="md"
+                  type="text"
+                  required
+                  id="name"
+                  value={profile.name}
+                  onChange={handleTextChange}
+                />
+                <Form.Label>Twitter</Form.Label>
+                <Form.Control
+                  className="mb-2"
+                  size="md"
+                  type="text"
+                  id="twitter"
+                  value={profile.twitter}
+                  onChange={handleTextChange}
+                />
+                <Form.Label>Instagram</Form.Label>
+                <Form.Control
+                  className="mb-2"
+                  size="md"
+                  type="text"
+                  id="instagram"
+                  value={profile.instagram}
+                  onChange={handleTextChange}
+                />
+                <Form.Label>Linkedin</Form.Label>
+                <Form.Control
+                  className="mb-2"
+                  size="md"
+                  type="text"
+                  id="linkedin"
+                  value={profile.linkedin}
+                  onChange={handleTextChange}
+                />
+                <Form.Label>Profile Picture</Form.Label>
+                <Form.Control
+                  className="mb-2"
+                  size="md"
+                  type="text"
+                  id="picture"
+                  value={profile.picture}
+                  onChange={handleTextChange}
+                />
+                <Form.Label>About Me</Form.Label>
+                <Form.Control
+                  className="mb-4"
+                  size="md"
+                  required
+                  as="textarea"
+                  id="about"
+                  value={profile.about}
+                  placeholder="Tell us about yourself!"
+                  onChange={handleTextChange}
+                />
+                <div className="d-grid px-0">
+                  <Button
+                    type="submit"
+                    style={{ maxWidth: "200px" }}
+                    variant="warning"
+                    size="md"
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </Row>
             </div>
-          </Row>
+          </main>
         </div>
-      </main>
+      </Form>
     </div>
-  </Form>
-</div>
-  )
+  );
 }
 
 export default NewProfileForm;
